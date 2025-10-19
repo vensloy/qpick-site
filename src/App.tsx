6a76ba3ee7077e661755c9cc3df7f2cddebd8176
Anton <komogorcevanton10@gmail.com>
2025-10-19 17:53:03 +0800

    feat: added header

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,32 @@
+import basket from './assets/img/header/basket.svg'
+import arrow from './assets/img/header/arrow.svg'
+import phone from './assets/img/header/phone.svg'
+import heart from './assets/img/header/heart.svg'
+
+import './css/header.css'
+
 const App = () => {
   return (
-    <></>
+    <>
+      <header>
+        <section className="header-left">
+          <h1 className="logo">QPICK</h1>
+
+          <div className="select-phone-model">
+            <img src={ phone } alt="" />
+            <p className='select-phone-model-text'>Выбрать модель телефона</p>
+            <img src={ arrow } alt="" />
+          </div>
+        </section>
+        
+        <section className='header-right'>
+          <nav>
+            <img src={ heart } alt="" />
+            <img src={ basket } alt="" />
+          </nav>
+        </section>
+      </header>
+    </>
   )
 }
 
